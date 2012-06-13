b9223f9eff453c372cf3754b0ff1424ab042b9ea
emopandas <cloud935@msn.com>
2012-06-13 03:13:33 -0500

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #return a title on per page basis
+
+  def title
+    base_title = "Welcome to my sample app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"  
+    end
+  end
 end
